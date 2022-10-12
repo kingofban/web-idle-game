555f9c00097bd938e82cd1cbd6e90ca929dbed22
kingofban <115199223+kingofban@users.noreply.github.com>
2022-10-11 20:26:01 -0500

    added upgrades

--- script.js
+++ script.js
@@ -21,15 +21,25 @@ $a("button").forEach((item)=>{
 
 // Display variables
 let cookiesEl=$("#cookies")
+let cookiesPerClickEl=$("#cookiesPerClick")
 
 function update() {
     cookiesEl.innerText=cookies;
+    cookiesPerClickEl.innerText=cookiesPerClick;
 }
 
 // Game variables
 let cookies=0;
+let cookiesPerClick=1;
 
 function getCookie() {
-    cookies++
+    cookies+=cookiesPerClick
     update()
 }
+
+function getUpgrade() {
+    cookiesPerClick++
+    update()
+}
+
+update();
